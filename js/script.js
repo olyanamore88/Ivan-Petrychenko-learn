38251ac7e1cccfe25a69d7777262766539cfc268
olyanamore88 <reschetiloolga@gmail.com>
2021-10-17 21:07:29 +0300

    for

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+ const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
 const personalMovieDB = {
    count: numberOfFilms,
@@ -8,12 +8,12 @@ const personalMovieDB = {
    privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++){
+   const  a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+          personalMovieDB.movies[a] = b;
+}
 
       console.log(personalMovieDB);
