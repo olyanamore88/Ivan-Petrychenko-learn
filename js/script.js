3dd97212833f8efd16e1eccadc14f80ce7250707
olyanamore88 <reschetiloolga@gmail.com>
2021-10-19 15:02:00 +0300

    if-02

--- js/script.js
+++ js/script.js
@@ -23,4 +23,14 @@ for (let i = 0; i < 2; i++){
          
 }
 
+if (personalMovieDB.count < 10) {
+   console.log("Просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count >=10 && personalMovieDB.count < 30) {
+   console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+   console.log("Вы киноман");
+} else {
+   console.log("Произошла ошибка");
+}
+
       console.log(personalMovieDB);
